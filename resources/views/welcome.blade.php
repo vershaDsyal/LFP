a131fbe2f6f2d5b94110477f90e4cb4a64443d8b
vershaDsyal <dhimanversha000@gmail.com>
2023-04-26 16:37:09 +0400

    adding LFP logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                  <!--  <img src="{{ asset('elc_theme/assets/images/lfp.jpg') }}" /> -->
+                 <img src="{{ asset('elc_theme/assets/images/lfp.jpg') }}" /> 
                 </div>
 
                 
