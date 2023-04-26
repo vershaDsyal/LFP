d12c43e21abb42d1daf80fae82290ada4840e3e6
vershaDsyal <dhimanversha000@gmail.com>
2023-04-26 12:06:48 +0400

    setting up theme

---
+++ resources/views/partials/topnav.blade.php
@@ -0,0 +1,38 @@
+<!-- navbar -->
+<!-- ============================================================== -->
+<div class="dashboard-header">
+    <nav class="navbar navbar-expand-lg bg-white fixed-top">
+        <span style="font-size: 22px;" class="customImage"><!-- <a class="navbar-brand" href="{{ url('/') }}"> <img src="{{ asset('elc_theme/assets/images/lfp.jpg') }}" width="125px" height="50px" /></a> --></span>
+
+         
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto navbar-right-top">
+                @if(\Auth::user())
+                <li class="nav-item dropdown nav-user">
+                    <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ asset('elc_theme/assets/images/default-user-image.png') }}" alt="" class="user-avatar-md rounded-circle"></a>
+                    <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
+                        <div class="nav-user-info">
+                            <h5 class="mb-0 text-white nav-user-name">
+                                <span class="badge-dot badge-success"></span>
+                                @if(\Auth::user()) {{ Auth::user()->name }} @endif<br>
+                            </h5>
+                        </div>
+                       
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endif
+
+            </ul>
+        </div>
+    </nav>
+</div>
+<!-- ============================================================== -->
+<!-- end navbar -->
