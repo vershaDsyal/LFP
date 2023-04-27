0c93922018bd2b33322a9546e1ed1df2b2d03bcb
vershaDsyal <dhimanversha000@gmail.com>
2023-04-27 16:19:23 +0400

    created migrations and controllers with mcr

---
+++ database/migrations/2023_04_27_120242_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('order_id')->nullable();
+            $table->bigInteger('customer_id')->nullable();
+            $table->text('price')->nullable();
+            $table->text('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
